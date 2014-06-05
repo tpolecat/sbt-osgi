c3d71a87aaba8b6ee7d7d0c7768d198a00c3e963
Rob Norris <rob_norris@mac.com>
2014-06-04 17:03:59 -0700

    explodedJars

--- src/main/scala/com/typesafe/sbt/osgi/Osgi.scala
+++ src/main/scala/com/typesafe/sbt/osgi/Osgi.scala
@@ -32,11 +32,12 @@ private object Osgi {
     artifactPath: File,
     resourceDirectories: Seq[File],
     embeddedJars: Seq[File],
+    explodedJars: Seq[File],
     streams: TaskStreams): File = {
     val builder = new Builder
     builder.setClasspath(fullClasspath map (_.data) toArray)
     builder.setProperties(headersToProperties(headers, additionalHeaders))
-    includeResourceProperty(resourceDirectories.filter(_.exists), embeddedJars) foreach (dirs =>
+    includeResourceProperty(resourceDirectories.filter(_.exists), embeddedJars, explodedJars) foreach (dirs =>
       builder.setProperty(INCLUDERESOURCE, dirs)
     )
     bundleClasspathProperty(embeddedJars) foreach (jars =>
@@ -77,8 +78,11 @@ private object Osgi {
   def seqToStrOpt[A](seq: Seq[A])(f: A => String): Option[String] =
     if (seq.isEmpty) None else Some(seq map f mkString ",")
 
-  def includeResourceProperty(resourceDirectories: Seq[File], embeddedJars: Seq[File]) =
-    seqToStrOpt(resourceDirectories ++ embeddedJars)(_.getAbsolutePath)
+  def includeResourceProperty(resourceDirectories: Seq[File], embeddedJars: Seq[File], explodedJars: Seq[File]) = {
+    val paths: Seq[String] =
+      (resourceDirectories ++ embeddedJars).map(_.getAbsolutePath) ++ explodedJars.map(f => "@" + f.getAbsolutePath)
+    seqToStrOpt(paths)(identity)
+  }
 
   def bundleClasspathProperty(embeddedJars: Seq[File]) =
     seqToStrOpt(embeddedJars)(_.getName) map (".," + _)
